60c764f0c694d19f476691cfa02741aef97d4097
Federico Feliziani <me@federicofeliziani.com>
2015-07-23 16:49:43 +0200

    FF: Added canvas width and height using prop rather than css. This is only temporary as this will have to work in any resolution as a background, but for now will do.

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
             // Init library
 			this.filter("canvas").each(function() {
 				var canvas = $(this);
+				canvas.prop({width: 530, height: 530});
 				var ctx = canvas.getContext("2d");
 				
 			});
